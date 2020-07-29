176a5a03063944ddc16accd2851eb5b13e8c4860
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-29 17:18:22 +0000

    added names

--- client.js
+++ client.js
@@ -12,6 +12,12 @@ const connect = function() {
   conn.on('data', (data) => {
   console.log('Server says: ', data);
 	})
+  conn.on('connect', () => {
+  console.log('Successfully connected to game server');
+  })
+  conn.on('connect', () => {
+  conn.write('Name: DLC');
+  });
 
   return conn;
 }
